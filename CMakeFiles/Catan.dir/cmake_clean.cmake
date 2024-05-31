048f3654400f8167a27bebd8e51bf3189122f793
GuyShimonN <guyguy845@gmail.com>
2024-05-31 19:04:17 +0300

    satrt

---
+++ CMakeFiles/Catan.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Catan.dir/src/Board.cpp.o"
+  "CMakeFiles/Catan.dir/src/Board.cpp.o.d"
+  "CMakeFiles/Catan.dir/src/Game.cpp.o"
+  "CMakeFiles/Catan.dir/src/Game.cpp.o.d"
+  "CMakeFiles/Catan.dir/src/Tile.cpp.o"
+  "CMakeFiles/Catan.dir/src/Tile.cpp.o.d"
+  "CMakeFiles/Catan.dir/src/main.cpp.o"
+  "CMakeFiles/Catan.dir/src/main.cpp.o.d"
+  "Catan"
+  "Catan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Catan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
